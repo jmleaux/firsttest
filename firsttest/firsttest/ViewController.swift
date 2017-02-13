24fcb27b7b31290b6638854371e0beb43e472a19
J. M. Lowe <jmlowe@Palladium.local>
2017-02-13 20:28:18 +0300

    second commit

---
+++ firsttest/firsttest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  firsttest
+//
+//  Created by J. M. Lowe on 2/13/17.
+//  Copyright © 2017 JMLeaux LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("This is my first test.")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
