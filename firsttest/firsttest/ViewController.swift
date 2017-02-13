459308fa78e89e59b57b3d83b3869404febad294
J. M. Lowe <jmlowe@Palladium.local>
2017-02-13 20:29:35 +0300

    third commit

--- firsttest/firsttest/ViewController.swift
+++ firsttest/firsttest/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("This is my first test.")
+        print("This is my second test line.")
     }
 
     override func didReceiveMemoryWarning() {
